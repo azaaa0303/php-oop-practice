ffac7c72e9b3047c371f56e4f7342b4c58d1bcd4
Аза <azatberdiev6@gmail.com>
2026-09-14 19:59:20 +0400

    Add User class with basic OOP

---
+++ book.php
@@ -0,0 +1,12 @@
+<?php
+class User {
+    private $login;
+    
+    public function __construct($login) {
+        $this->login = $login;
+    }
+    
+    public function getLogin() {
+        return $this->login;
+    }
+}
