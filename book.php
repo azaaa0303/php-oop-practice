a6febd5a0b31a487a37e474a34c334eebc46b300
Аза <azatberdiev6@gmail.com>
2026-09-16 09:58:26 +0400

    first commit

--- book.php
+++ book.php
@@ -1,12 +1,19 @@
 <?php
 class User {
     private $login;
-    
-    public function __construct($login) {
+    private $role;
+
+    public function __construct($login, $role = 'user') {
         $this->login = $login;
+        $this->role = $role;
     }
-    
     public function getLogin() {
         return $this->login;
+    }
+    public function getRole(){
+        return $this->role;
+    }
+    public function getDescription(){
+        return "Пользователь " . $this->login . ", роль " . $this->role;
     }
 }
--- index.php
+++ index.php
@@ -1,2 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 require "book.php";
+?>
+<?php $user1 = new User('Вася'); ?>
+<?php $user2 = new User('petya'); ?>
+<?php $user3 = new User('Ivan', 'admin'); ?> 
+
+
+<?php echo $user1 -> getDescription();?><br>
+<?php echo $user2 -> getDescription();?><br>
+<?php echo $user3 -> getDescription();?><br>
+
+
+</body>
+</html>
